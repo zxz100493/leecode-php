53c707a7b99fb58d76913682e56674348fad9f24
zxz100493 <zxz100493@163.com>
2023-09-02 09:20:36 +0800

    bad version

---
+++ leecode278.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=278 lang=php
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+/* The isBadVersion API is defined in the parent class VersionControl.
+public function isBadVersion($version){} */
+
+class VersionControl
+{
+    public function isBadVersion($version)
+    {
+        if ($version >= 6) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class Solution extends VersionControl
+{
+    /**
+     * @param Int $n
+     * @return Int
+     */
+    public function firstBadVersion($n)
+    {
+        var_dump($n);
+        $num = round($n / 2);
+        var_dump($num);
+        $left = 1;
+        $right = $n;
+        while ($left < $right) {
+            $mid = $left + ($right - $left) / 2;
+            var_dump($mid);die;
+            if ($this->isBadVersion($mid)) {
+                $right = $mid;
+            } else {
+                $left = $mid + 1;
+            }
+        }
+        return $left;
+    }
+}
+$res = (new Solution())->firstBadVersion(10);
+// @lc code=end
