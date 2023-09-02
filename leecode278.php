f67a82b053f6b6d5ebd1eab47433ba58b9e987d3
zxz100493 <zxz100493@163.com>
2023-09-02 09:21:36 +0800

    edit code

--- leecode278.php
+++ leecode278.php
@@ -29,13 +29,10 @@ class Solution extends VersionControl
      */
     public function firstBadVersion($n)
     {
-        var_dump($n);
-        $num = round($n / 2);
-        var_dump($num);
         $left = 1;
         $right = $n;
         while ($left < $right) {
-            $mid = $left + ($right - $left) / 2;
+            $mid = (int) ($left + ($right - $left) / 2);
             var_dump($mid);die;
             if ($this->isBadVersion($mid)) {
                 $right = $mid;
